b534086e93b602e1cfaf7b7050692e9ca21d39da
Benjamin Walker <132629557+bwalkerl@users.noreply.github.com>
2024-05-24 15:57:31 +1000

    Change hook callback to string (#211)

--- db/hooks.php
+++ db/hooks.php
@@ -28,7 +28,7 @@ defined('MOODLE_INTERNAL') || die();
 $callbacks = [
     [
         'hook' => \core\hook\output\before_standard_top_of_body_html_generation::class,
-        'callback' => [\local_envbar\hook_callbacks::class, 'before_standard_top_of_body_html_generation'],
+        'callback' => '\local_envbar\hook_callbacks::before_standard_top_of_body_html_generation',
         'priority' => 0,
     ],
 ];
