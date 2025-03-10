81dba8913ca6ec0d93e4e15397fd3a6a97d92d92
Jason den Dulk <jason@jaypha.com.au>
2025-03-10 14:44:24 +1100

    Issue #235: Add check for overdue refreshes. (#237)
    Co-authored-by: Jason Den Dulk <jasondendulk@your.hostname.com>

--- version.php
+++ version.php
@@ -27,7 +27,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2025030600;      // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2025030700;      // The current plugin version (Date: YYYYMMDDXX).
 $plugin->release   = 2025011700;      // Same as version
 $plugin->requires  = 2024100700;      // Requires Moodle 4.5 or later.
 $plugin->component = 'local_envbar';
