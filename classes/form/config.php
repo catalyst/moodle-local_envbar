b5f9f9bd068f6974445fc992ecee689fcdedfa4d
Dmitrii Metelkin <dmitriim@catalyst-au.net>
2025-09-16 10:22:02 +1000

    Issue#253: Use empty function

--- classes/form/config.php
+++ classes/form/config.php
@@ -111,7 +111,7 @@ class config extends moodleform {
         $mform->setType("prodwwwroot", PARAM_URL);
         $mform->setDefault("prodwwwroot", envbarlib::getprodwwwroot());
 
-        if ($CFG->allowmultipledomains ?? false) {
+        if (!empty($CFG->allowmultipledomains)) {
             $mform->addElement(
                 'textarea',
                 'secondaryurls',
--- classes/local/envbarlib.php
+++ classes/local/envbarlib.php
@@ -326,7 +326,7 @@ CSS;
             }
 
             // Do not display on the production secondary urls!
-            if ($CFG->allowmultipledomains ?? false) {
+            if (!empty($CFG->allowmultipledomains)) {
                 $customdomains = explode("\n", self::getprodsecondaryurls());
                 foreach ($customdomains as $customdomain) {
                     if ($customdomain === $CFG->wwwroot) {
