2ae36fc9ee60afbee5f3a574beb4729c797196dc
Brendan Heywood <brendan@catalyst-au.net>
2026-06-25 13:21:50 +1000

    Bump version

--- version.php
+++ version.php
@@ -27,8 +27,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2025111801;      // The current plugin version (Date: YYYYMMDDXX).
-$plugin->release   = 2025111801;      // Same as version.
+$plugin->version   = 2026062501;      // The current plugin version (Date: YYYYMMDDXX).
+$plugin->release   = 2026062501;      // Same as version.
 $plugin->requires  = 2025042200;      // Requires Moodle 5.0 or later.
 $plugin->component = 'local_envbar';
 $plugin->maturity  = MATURITY_STABLE;
